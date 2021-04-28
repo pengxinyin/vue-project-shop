ddf6210c06b21094ab74e2d3ccc6097f5d16e645
pengxinyin <1551946489@qq.com>
2021-04-28 17:05:58 +0800

    登录提示框

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import MyHttpServer from '@/plugins/http.js'
 
 // 引入element-ui文件
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/reset.css'
+
 Vue.use(ElementUI)
+Vue.use(MyHttpServer)
 
 Vue.config.productionTip = false
 
