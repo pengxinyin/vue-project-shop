edc3624be1e6bc04335e8a423e2c7cddd38c482e
pengxinyin <1551946489@qq.com>
2021-04-28 17:11:48 +0800

    显示home组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '../components/login/Login.vue'
-
+import Login from '@/components/login/Login.vue'
+import Home from '@/components/home/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name:'Home',
+      component:Home
+    },
     {
       path: '/login',
       name:'Login',
